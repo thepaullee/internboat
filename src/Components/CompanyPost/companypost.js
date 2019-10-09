8c78077adc71fff15f11078decc7cff30613fdce
Paul Lee <plee032@uottawa.ca>
2019-10-09 14:19:19 -0400

    commit

--- src/Components/CompanyPost/companypost.js
+++ src/Components/CompanyPost/companypost.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import {Card, CardBody, CardTitle, CardSubtitle, CardHeader} from 'shards-react';
+import {Card, CardBody, CardDeck, CardSubtitle, CardHeader, CardTitle} from 'shards-react';
 import './companypost.css';
 
 class CompanyPost extends React.Component{
@@ -19,40 +19,36 @@ class CompanyPost extends React.Component{
 
         return(
             <div className="companyPost">
-                <Card>
-                </Card>
+                <CardDeck className="company-cards">
 
 
                     {Object.values(this.state.companies).map((key, index) => 
-                    <div>
-                        <Card className="company-card">
-                            <CardHeader>{key.companyName}</CardHeader>
-                            <CardBody>
+                    <div key={index}>
+                        <Card style={{width: "300px", height:"300px"}}>
+                            <CardHeader>
+                                <CardTitle>{key.companyName}</CardTitle><br/>
                                 <CardSubtitle>Applications: {key.isHiring ? "OPEN":"CLOSED"}</CardSubtitle>
-                                <p>Countries: {key.countries}</p>
+
+                            </CardHeader>
+                            
+                            <CardBody>
+                                <CardSubtitle>Regions:</CardSubtitle>
+                                <p>{key.countries.map((value, index) =>
+                                        {return <p key={index}>{value}</p>})}
+                                </p>
+
+
                                 <CardSubtitle>Hiring Periods</CardSubtitle>
-                                <p>Fall: {key.hiringPeriods.fall ? "Yes":"False"}</p>
-                                <p>Winter: {key.hiringPeriods.winter ? "Yes":"False"}</p>
-                                <p>Summer: {key.hiringPeriods.summer ? "Yes":"False"}</p>
-                                <h4>Applications: {key.isHiring ? "Open":"Closed"}</h4>
+                                <p>Fall: {key.hiringPeriods.fall ? "Yes":"False"}<br></br>
+                                Winter: {key.hiringPeriods.winter ? "Yes":"False"}<br></br>
+                                Summer: {key.hiringPeriods.summer ? "Yes":"False"}</p>
                                 <a href={key.internshipPage} target="_blank" rel="noopener noreferrer">Internship Page</a>
 
                             </CardBody>
                         </Card>
                     </div>
-
-                    /*<div>
-                        <h1>{key.companyName}</h1>
-                        <h4>Countries: {key.countries}</h4>
-                        <h4>Hiring Periods</h4>
-                        <p>Fall: {key.hiringPeriods.fall ? "Yes":"False"}</p>
-                        <p>Winter: {key.hiringPeriods.winter ? "Yes":"False"}</p>
-                        <p>Summer: {key.hiringPeriods.summer ? "Yes":"False"}</p>
-                        <h4>Applications: {key.isHiring ? "Open":"Closed"}</h4>
-                        <a href="{key.internshipPage}">Internship Page</a>
-                    </div>*/
                     )}
-                
+                </CardDeck>
             </div>
         );
     }
---
+++ src/Components/SignUp/signup.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+import * as ROUTES from '../../constants/routes/';
+
+const SignUpPage = () => {
+    
+}
